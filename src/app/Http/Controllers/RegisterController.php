ffea4750428d1393cf69d5c1c3a9e4cd9397cd86
aki <h.aki5538@gmail.com>
2026-01-09 20:21:09 +0900

    vender削除

--- src/app/Http/Controllers/RegisterController.php
+++ src/app/Http/Controllers/RegisterController.php
@@ -18,6 +18,7 @@ class RegisterController extends Controller
             'name' => $validated['name'],
             'email' => $validated['email'],
             'password' => bcrypt($validated['password']),
+            'postal_code' => $request->postal_code,
         ]);
 
         // メール送信のトリガー
